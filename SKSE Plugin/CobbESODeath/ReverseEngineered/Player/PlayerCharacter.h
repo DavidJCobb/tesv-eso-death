b8ba0e5da9aedfaa56fc5447e413070fd66c6ca4
DavidJCobb <DavJCobb@gmail.com>
2018-08-07 22:28:17 +0200

    v1.1
    Reverse-engineering for PlayerSkills; some findings for PlayerCharacter, such as last-known outdoor position.

--- SKSE Plugin/CobbESODeath/ReverseEngineered/Player/PlayerCharacter.h
+++ SKSE Plugin/CobbESODeath/ReverseEngineered/Player/PlayerCharacter.h
@@ -7,6 +7,9 @@ namespace RE {
    class PlayerCharacter : public Character { // sizeof == 0x72C
       public:
          virtual ~PlayerCharacter();
+         // virtual SInt32 GetBountyViolent(TESFaction*);    // Virtual 0x12A
+         // virtual SInt32 GetBountyNonViolent(TESFaction*); // Virtual 0x12B
+         // virtual void   Unk_12C(TESFaction*);             // Virtual 0x12C // related to paying the player's bounty
 
          struct Unk494 {
             float  unk00 = -1;
@@ -29,9 +32,9 @@ namespace RE {
          NiPoint3 unk2BC; // 2BC - initialized from (NiPoint3*)0x01B910A4
          UInt32   unk2C8;
          UInt32   unk2CC;
-         UnkFormArray unk2D0;
+         UnkFormArray unk2D0; // perks?
          UnkFormArray unk2DC;
-         UnkFormArray unk2E8;
+         UnkFormArray unk2E8; // perks?
          UnkFormArray unk2F4;
          UnkFormArray unk300;
          UnkFormArray unk30C;
@@ -40,12 +43,13 @@ namespace RE {
          UInt32       unk330 = 0;
          UInt32       unk334 = 0;
          UnkFormArray unk338;
-         UInt32 unk344;
-         UInt32 unk348;
+         float  unk344;
+         float  unk348;
          UInt32 unk34C = 0;
          UInt32 unk350 = 0;
          UInt32 unk354 = 0;
          UInt32 unk358 = 0xDEADBEEF;
+         float  unk35C;
          UInt32 unk360 = 0;
          UInt32 unk364;
          UInt32 unk368;
@@ -64,10 +68,10 @@ namespace RE {
          UInt32 unk394;        //
          UInt32 unk398 = 0;    // ...end of.
          UInt32 unk39C = 0;
-         NiPoint3 unk3A0; // 3A0 // type assumed; all values initialized to 0x7F7FFFFF
-         struct {
+         NiPoint3 unk3A0; // 3A0 // all values initialized to 0x7F7FFFFF; seems to be the player's last-known coordinates in their last-known worldspace
+         struct Unk3AC {
             UInt32 unk00 = 0;
-            UInt32 unk04 = 0;
+            TESObjectCELL* unk04 = nullptr; // 04
             NiPoint3 unk08; // initialized to (NiPoint3*)0x01B910A4
             NiPoint3 unk14; // initialized to (NiPoint3*)0x01B910A4
             UInt32 unk20 = 0;
@@ -77,9 +81,11 @@ namespace RE {
             bool   unk30 = false;
             bool   unk31 = true;
             bool   unk32 = true;
+            UInt8  unk33; // padding?
+            //
+            MEMBER_FN_PREFIX(Unk3AC);
+            DEFINE_MEMBER_FN(Subroutine007475F0, void, 0x007475F0); // reset?
          } unk3AC; // this struct layout is used elsewhere, but it doesn't have VTBL/RTTI, so I can't ID it
-         UInt8 unk3DE;
-         UInt8 unk3DF;
          UInt32 unk3E0 = -1;
          UInt32 unk3E4 = 0; // these could be individual bytes
          UInt32 unk3E8 = 0;
@@ -94,21 +100,29 @@ namespace RE {
          UInt32 unk420 = 0xFF7FFFFF;
          UInt32 unk424;
          UInt32 unk428;
-         UInt32 unk42C = 0;
+         TESFaction* unk42C = nullptr; // 42C // faction currently arresting the player?
          UInt32 unk430 = 0;
          UInt32 unk434 = 0;
          UInt32 unk438[(0x48C - 0x438) / 4];
          UInt32 unk48C = 0;
          UInt32	unk490;							// 490 - Handle // own handle?
          Unk494 unk494[0xE];                    // 494; sizeof each is 0xC
-         UInt32	pad53C[(0x568 - 0x53C) >> 2];	// 53C
-         UInt32	unk568;							// 568 - Handle
+         UInt32	pad53C[(0x54C - 0x53C) >> 2]; // 53C
+         UInt32	unk54C; // 54C - Handle; PlayerCharacter::Subroutine0073D4B0 returns the actor and clears the handle
+         UInt32	pad550[(0x568 - 0x550) >> 2];
+         UInt32	unk568; // 568 - Handle
          UInt32	pad56C[(0x58C - 0x56C) >> 2];	// 56C
-         NiNode* firstPersonSkeleton;			// 58C
+         NiNode*  firstPersonSkeleton; // 58C
          UInt32	pad590[(0x5AC - 0x590) >> 2];
          UInt32	lastRiddenHorseHandle;			// 5AC - Handle
-         UInt32	pad5B0[(0x614 - 0x5B0) >> 2];
-         PlayerSkills*	skills;					// 614
+         UInt32	pad5B0[(0x5FC - 0x5B0) >> 2];
+         TESObjectCELL* unk5FC; // 5FC // redundant player cell? can be an interior.
+         UInt32   unk600;
+         UInt32   unk604;
+         UInt32   unk608;
+         UInt32   unk60C;
+         UInt32   unk610;
+         PlayerSkills* skills;					// 614
          UInt32	targetHandle;					// 618
          UInt32	unk61C;							// 61C
          UInt32	unk620;							// 620
@@ -120,13 +134,15 @@ namespace RE {
          UInt32	unk640;							// 640
          TESForm* tempPoison;					// 644
          UInt32	numTeammates;					// 648
-         UInt32	pad64C[(0x6E0 - 0x64C) >> 2];
+         UInt32	pad64C[(0x6B0 - 0x64C) >> 2];
+         BGSLocation* unk6B0; // 6B0
+         UInt32	pad6B4[(0x6E0 - 0x6B4) >> 2];
          UInt8	unk6E0;							// 6E0
          UInt8	numPerkPoints;					// 6E1
          UInt16 unk6E2;							// 6E2
          UInt32	unk6E4;							// 6E4
          tArray<TintMask*>	tintMasks;			// 6E8		// These are the actual tints
-         tArray<TintMask*>* overlayTintMasks;	// 6F4		// These apply when overlay head parts is enabled
+         tArray<TintMask*> overlayTintMasks;	// 6F4		// These apply when overlay head parts is enabled
          UInt32 unk700 = 0;
          UInt32 unk704 = 0;
          UInt32 unk708 = 0x80000000;
@@ -162,13 +178,28 @@ namespace RE {
          };*/
 
          MEMBER_FN_PREFIX(PlayerCharacter);
-         DEFINE_MEMBER_FN(GetTintList, tArray <TintMask *> *, 0x0055FF90);
+         DEFINE_MEMBER_FN(GetTintList, tArray<TintMask*>*, 0x0055FF90);
          DEFINE_MEMBER_FN(GetNumTints, UInt32, 0x00735F40, UInt32 tintType);
-         DEFINE_MEMBER_FN(GetTintMask, TintMask *, 0x00735F00, UInt32 tintType, UInt32 index);
+         DEFINE_MEMBER_FN(GetTintMask, TintMask*, 0x00735F00, UInt32 tintType, UInt32 index);
          DEFINE_MEMBER_FN(GetDamage, double, 0x00730810, InventoryEntryData * pForm);
          DEFINE_MEMBER_FN(GetArmorValue, double, 0x007307E0, InventoryEntryData * pForm);
+         //
+         DEFINE_MEMBER_FN(GetInfamy,                 SInt32, 0x00744100, TESFaction*); // get lifetime bounty for faction
+         DEFINE_MEMBER_FN(GetInfamyViolent,          SInt32, 0x00744160, TESFaction*); // get lifetime bounty for faction (violent crimes)
+         DEFINE_MEMBER_FN(GetInfamyNonViolent,       SInt32, 0x007441B0, TESFaction*); // get lifetime bounty for faction (non-violent crimes)
+         DEFINE_MEMBER_FN(GetStolenItemValueCrime,   SInt32, 0x00744360, TESFaction*); // total value of all items the player was witnessed stealing from the faction
+         DEFINE_MEMBER_FN(GetStolenItemValueNoCrime, SInt32, 0x00744310, TESFaction*); // total value of all items the player stole from the faction without being caught
+         //
          DEFINE_MEMBER_FN(HandleLowHealthHeartbeat,          void, 0x00671690, float healthPercentage);
          DEFINE_MEMBER_FN(HandleLowHealthImagespaceModifier, void, 0x00733A60, float healthPercentage);
          DEFINE_MEMBER_FN(HandleLowStatTutorials, void, 0x00737C90, UInt32 avIndex, float avValuePercentage);
    };
+   static_assert(sizeof(PlayerCharacter) <= 0x72C, "RE::PlayerCharacter is too large.");
+   static_assert(sizeof(PlayerCharacter) >= 0x72C, "RE::PlayerCharacter is too small.");
+   static_assert(offsetof(PlayerCharacter, unk42C) <= 0x42C, "RE::PlayerCharacter::unk42C is too late.");
+   static_assert(offsetof(PlayerCharacter, unk42C) >= 0x42C, "RE::PlayerCharacter::unk42C is too early.");
+   static_assert(offsetof(PlayerCharacter, lastRiddenHorseHandle) <= 0x5AC, "RE::PlayerCharacter::lastRiddenHorseHandle is too late.");
+   static_assert(offsetof(PlayerCharacter, lastRiddenHorseHandle) >= 0x5AC, "RE::PlayerCharacter::lastRiddenHorseHandle is too early.");
+   static_assert(offsetof(PlayerCharacter, skills) <= 0x614, "RE::PlayerCharacter::skills is too late.");
+   static_assert(offsetof(PlayerCharacter, skills) >= 0x614, "RE::PlayerCharacter::skills is too early.");
 };
